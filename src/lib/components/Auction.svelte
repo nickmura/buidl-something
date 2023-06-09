98bfe6ab901065d9fdafb739e6b8585c78d51575
Safwan Rahman <safwanrahman9@gmail.com>
2023-06-09 10:43:37 -0600

    Co-authored-by: nickmura <nickmura@users.noreply.github.com>

--- src/lib/components/Auction.svelte
+++ src/lib/components/Auction.svelte
@@ -6,17 +6,56 @@
         create = !create;
     }
 
+    let auction = {
+        name: '',
+        description: '',
+        startingBid: '',
+        duration: '', 
+        currentBid: '',
+    }   
+    
 
 </script>
-    
-    
- {#if !create}   
+
+
+{#if !create}   
     <button on:click={toggleCreate} class="p-3 rounded-xl text-xl bg-black text-white font-semibold hover:scale-[1.05] transition transition-200">
         Create New Auction
-    </button>   
+    </button> 
 {:else}
-    <input id="name" class="border-2 border-black" placeholder="Name Your Auction">
-    <input id="startingBid" class="border-2 border-black" placeholder="Starting Bid">
-    <input id="description" class="border-2 border-black" placeholder="Description">
-    <input id="timeLimit" class="border-2 border-black" placeholder="Description">
+
+<div class="border-10  p-10 rounded-xl gap-6 flex flex-row ">
+   
+    
+      
+    <div class="flex flex-col gap-6">
+    <input bind:value={auction.name} class="inputs" placeholder="Name Your Auction">
+    <input bind:value={auction.description} class="inputs" placeholder="Description">
+    <input bind:value={auction.startingBid} type="number" class="inputs" placeholder="Starting Bid">
+    <input bind:value={auction.duration} type="number" class="inputs" placeholder="Duration">
+
+    <button on:click={toggleCreate} class="p-3 rounded-xl text-xl bg-black text-white font-semibold hover:scale-[1.05] transition transition-200">
+        Create
+    </button> 
+    </div>
+
+    <div class="w-80 border-black border-2 rounded-xl flex flex-col">
+        <p class="self-center">Preview</p>
+    
+    
+    
+    
+    
+    </div>
+</div>
 {/if}
+
+
+    
+<style> 
+
+.inputs{
+    @apply rounded-xl border-black border-2  p-2 
+}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,18 +9,17 @@
 	} from '$lib/stores/provider';
 	import { balanceOnBlock } from '$lib/stores/state';
 	import { formatEther } from 'ethers/lib/utils';
+	import Auction from '$lib/components/Auction.svelte'
 </script>
 
 <main class="flex flex-col p-10 w-screen h-screen items-center gap-5 font-serif">
 	<h1 class="text-5xl text-black font-bold">buidl-something</h1>
 
-	<section class="flex flex-col p-10 items-start gap-5 border-2 rounded-xl">
-		<h1 class="text-2xl text-black font-bold">Wallet and Provider</h1>
+	<section class="flex flex-row p-10 items-start gap-5 border-2 rounded-xl">
+		
 		<h2 class="text-xl text-black font-semibold">$connected: {$connected}</h2>
-		<h2 class="text-xl text-black font-semibold">$walletAddress: {$walletAddress}</h2>
-		<h2 class="text-xl text-black font-semibold">
-			$accountChainId: {JSON.stringify($accountChainId)}
-		</h2>
+	
+	
 		<button
 			on:click={connectMetamask}
 			class="p-3 rounded-xl text-xl bg-black text-white font-semibold hover:scale-[1.05] transition transition-200"
@@ -37,14 +36,12 @@
 			>Disconnect</button
 		>
 	</section>
-	<section class="flex flex-col p-10 items-start gap-5 border-2 rounded-xl">
-		<h1 class="text-2xl text-black font-bold">Sync contract state on block, event or interval</h1>
-		<p class="text-md text-black font-semibold">
-			Set In '$lib/globalState.ts', will load as long as a store is used from '$lib/stores/state.ts'
-			or you import the globalState in the root of your project with 'import "$lib/globalState";'
-		</p>
-		<h2 class="text-xl text-black font-semibold">
-			$balanceOnBlock: {formatEther($balanceOnBlock)} ETH
-		</h2>
-	</section>
+	
+
+
+	<Auction></Auction>
+
+
+
+
 </main>
